379ed31e4545a5cfece4d5ea59c47d39efd89224
kampon <kampon.ktb@gmail.com>
2018-12-20 22:42:56 +0700

    Add unittest, case TestCaptchaCasePatternEqual1AndOperand1Equal1AndOperatorEqual2AndOperand2Equal4

--- main/captcha_test.go
+++ main/captcha_test.go
@@ -114,3 +114,11 @@ func TestCaptchaCasePatternEqual1AndOperand1Equal1AndOperatorEqual2AndOperand2Eq
 		t.Errorf("expect result: %s, actual: %s", expect, result)
 	}
 }
+func TestCaptchaCasePatternEqual1AndOperand1Equal1AndOperatorEqual2AndOperand2Equal4(t *testing.T) {
+	expect := "1 - four"
+	result := main.Captcha(1, 1, 2, 4)
+
+	if result != expect {
+		t.Errorf("expect result: %s, actual: %s", expect, result)
+	}
+}
